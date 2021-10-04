403ed014b60b359b17a3486d4b20f452865c92f8
Leonardo Pantaleon <lpant011@fiu.edu>
2021-10-04 01:16:33 -0400

    added only moving to congratulations on successful account creation

--- components/SignupPhone/SignupPhone.js
+++ components/SignupPhone/SignupPhone.js
@@ -2,16 +2,16 @@ import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
 const axios = require("axios");
 import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  Keyboard,
-  TouchableOpacity,
-  KeyboardAvoidingView,
-  ScrollView,
-  TextInput,
-  Platform,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    Keyboard,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    ScrollView,
+    TextInput,
+    Platform,
 } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 import { NavigationContainer } from "@react-navigation/native";
@@ -21,197 +21,213 @@ import { ColorAndroid } from "react-native/Libraries/StyleSheet/PlatformColorVal
 import { withSafeAreaInsets } from "react-native-safe-area-context";
 
 const SignupEmail = (props) => {
-  const { route, navigation } = props;
-  let { email, password, firstname, lastname } = route.params
+    const { route, navigation } = props;
+    let { email, password, firstname, lastname } = route.params;
 
-    email = String(email)
-    password = String(password)
-    firstname = String(firstname)
-    lastname = String(lastname)
-    
+    email = String(email);
+    password = String(password);
+    firstname = String(firstname);
+    lastname = String(lastname);
 
-  const [loaded] = useFonts({
-    Regular: require("../../assets/fonts/SF-Pro-Display-Regular.otf"),
-    Semibold: require("../../assets/fonts/SF-Pro-Display-Semibold.otf"),
-    Bold: require("../../assets/fonts/SF-Pro-Display-Bold.otf"),
-    Medium: require("../../assets/fonts/SF-Pro-Display-Medium.otf"),
-  });
+    const [loaded] = useFonts({
+        Regular: require("../../assets/fonts/SF-Pro-Display-Regular.otf"),
+        Semibold: require("../../assets/fonts/SF-Pro-Display-Semibold.otf"),
+        Bold: require("../../assets/fonts/SF-Pro-Display-Bold.otf"),
+        Medium: require("../../assets/fonts/SF-Pro-Display-Medium.otf"),
+    });
 
-  const [phoneNumber, setPhoneNumber] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [accountFailedCreation, setAccountFailedCreation] = useState(false);
 
-  const handleSubmit = (e) => {
-    if (phoneNumber != "") {
-        console.log(email)
-        axios({
-            method: "post",
-            url: "https://business-card-backend-qkym9.ondigitalocean.app/createuser",
-            data: {
-                firstname: firstname,
-                lastname: lastname,
-                email: email,
-                userpassword: password,
-                username: "DavyThebornboZEDGOD2345BIGHAIR",
-            }
-        })
-            .then(function (response) {
-                console.log(response);
-                navigation.navigate("AccountCreated", {
-                    email: email,
-                    password: password,
+    const accountFailed = () => {};
+
+    const handleSubmit = (e) => {
+        if (phoneNumber != "") {
+            console.log(email);
+            axios({
+                method: "post",
+                url: "https://business-card-backend-qkym9.ondigitalocean.app/createuser",
+                data: {
                     firstname: firstname,
                     lastname: lastname,
-                    phonenumber: phoneNumber
-                })
+                    email: email,
+                    userpassword: password,
+                    username: "DavyThebornboZEDGOD2345BIGHAIRegrerg",
+                },
             })
-            .catch(function (error) {
-                if (error.response) {
-                    // Request made and server responded
-                    if (error.response.data === "Username is taken") {
-                        setUsernameError(true);
-                    } else if (error.response.data === "Email is taken") {
-                        setEmailError(true);
+                .then(function (response) {
+                    if (response.status === 200) {
+                        navigation.navigate("AccountCreated", {
+                            email: email,
+                            password: password,
+                            firstname: firstname,
+                            lastname: lastname,
+                            phonenumber: phoneNumber,
+                        });
+                    } else {
+                        setAccountFailedCreation(true);
+                        console.log(response.status);
                     }
-                } else if (error.request) {
-                    // The request was made but no response was received
-                    console.log(error.request);
-                } else {
-                    // Something happened in setting up the request that triggered an Error
-                    console.log("Error", error.message);
-                }
-            });
-    } else {
-        console.log("No phone number provided");
-    }
-  };
+                })
+                .catch(function (error) {
+                    if (error.response) {
+                        // Request made and server responded
+                        if (error.response.data === "Username is taken") {
+                            setUsernameError(true);
+                        } else if (error.response.data === "Email is taken") {
+                            setEmailError(true);
+                        }
+                    } else if (error.request) {
+                        // The request was made but no response was received
+                        console.log(error.request);
+                    } else {
+                        // Something happened in setting up the request that triggered an Error
+                        console.log("Error", error.message);
+                    }
+                });
+        } else {
+            console.log("No phone number provided");
+        }
+    };
 
-  return (
-    <KeyboardAwareScrollView
-      keyboardShouldPersistTaps="always"
-      scrollEnabled={false}
-      bounces={false}
-      contentContainerStyle={styles.container}
-    >
-      <StatusBar style="light" />
-      <View style={styles.main}>
-        <View>
-          <View>
-            <Text style={styles.h1}>What is your phone number?</Text>
-          </View>
-          <View>
-            <TextInput
-            keyboardType="number-pad"
-              autoCompleteType="off"
-              autoCorrect={false}
-              keyboardAppearance="dark"
-              enablesReturnKeyAutomatically={true}
-              returnKeyType="next"
-              returnKeyLabel="continue"
-              onSubmitEditing={() => {handleSubmit()}}
-              autoFocus={true}
-              style={styles.input}
-              placeholder="Phone Number"
-              placeholderTextColor="rgba(235,235,245,.30)"
-              onChangeText={(phoneNumber) => setPhoneNumber(phoneNumber)}
-            />
-          </View>
-        </View>
-        {/** Validate if user entered an phoneNumber */}
-        {phoneNumber === "" && (
-          <TouchableOpacity
-            disabled={true}
-            onPress={() => navigation.navigate("AccountCreated", {
-                email: email,
-                password: password,
-                firstname: firstname,
-                lastname: lastname,
-                phonenumber: phoneNumber 
-            })}
-          >
-            <Text style={styles.btn}>Continue</Text>
-          </TouchableOpacity>
-        )}
-        {phoneNumber != "" && (
-          <TouchableOpacity
-            onPress={() => {handleSubmit()}}
-          >
-            <Text style={styles.btnActive}>Continue</Text>
-          </TouchableOpacity>
-        )}
-      </View>
-    </KeyboardAwareScrollView>
-  );
+    return (
+        <KeyboardAwareScrollView
+            keyboardShouldPersistTaps="always"
+            scrollEnabled={false}
+            bounces={false}
+            contentContainerStyle={styles.container}
+        >
+            <StatusBar style="light" />
+            <View style={styles.main}>
+                <View>
+                    <View>
+                        <Text style={styles.h1}>
+                            What is your phone number?
+                        </Text>
+                    </View>
+                    <View>
+                        <TextInput
+                            keyboardType="number-pad"
+                            autoCompleteType="off"
+                            autoCorrect={false}
+                            keyboardAppearance="dark"
+                            enablesReturnKeyAutomatically={true}
+                            returnKeyType="next"
+                            returnKeyLabel="continue"
+                            onSubmitEditing={() => {
+                                handleSubmit();
+                            }}
+                            autoFocus={true}
+                            style={styles.input}
+                            placeholder="Phone Number"
+                            placeholderTextColor="rgba(235,235,245,.30)"
+                            onChangeText={(phoneNumber) =>
+                                setPhoneNumber(phoneNumber)
+                            }
+                        />
+                    </View>
+                </View>
+                {/** Validate if user entered an phoneNumber */}
+                {phoneNumber === "" && (
+                    <TouchableOpacity
+                        disabled={true}
+                        onPress={() =>
+                            navigation.navigate("AccountCreated", {
+                                email: email,
+                                password: password,
+                                firstname: firstname,
+                                lastname: lastname,
+                                phonenumber: phoneNumber,
+                            })
+                        }
+                    >
+                        <Text style={styles.btn}>Continue</Text>
+                    </TouchableOpacity>
+                )}
+                {phoneNumber != "" && (
+                    <TouchableOpacity
+                        onPress={() => {
+                            handleSubmit();
+                        }}
+                    >
+                        <Text style={styles.btnActive}>Continue</Text>
+                    </TouchableOpacity>
+                )}
+            </View>
+        </KeyboardAwareScrollView>
+    );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#000000",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 10,
-  },
-  main: {
-    backgroundColor: "#000000",
-    alignItems: "center",
-    width: "100%",
-  },
-  h1: {
-    color: "#AEAEB2",
-    fontFamily: "Medium",
-    fontSize: 24,
-    textAlign: "center",
-    marginTop: -180,
-    paddingHorizontal: 10
-  },
-  input: {
-    width: "100%",
-    fontFamily: "Medium",
-    fontSize: 17,
-    borderStyle: "solid",
-    borderWidth: 0.5,
-    borderBottomColor: "rgba(84,84,88,.65)",
-    paddingLeft: 10,
-    paddingBottom: 10,
-    paddingTop: 10,
-    marginBottom: 125,
-    color: "#FFFFFF",
-  },
-  already: {
-    color: "rgba(0,122,255, 100)",
-    fontSize: 14,
-    fontFamily: "Bold",
-    paddingTop: 20,
-    paddingBottom: 15,
-    marginBottom: 70,
-    textAlign: "center",
-  },
-  btn: {
-    backgroundColor: "rgba(255,255,255,0.2)",
-    fontFamily: "Semibold",
-    fontSize: 17,
-    textAlign: "center",
-    paddingVertical: 15,
-    paddingHorizontal: 130,
-    borderRadius: 20,
-    overflow: "hidden",
-    width: "100%",
-  },
-  btncontainer: {
-    position: "absolute",
-    bottom: 0,
-    width: "100%",
-  },
-  btnActive: {
-    backgroundColor: "rgba(255,255,255,100)",
-    fontFamily: "Semibold",
-    fontSize: 17,
-    textAlign: "center",
-    paddingVertical: 15,
-    paddingHorizontal: 130,
-    borderRadius: 20,
-    overflow: "hidden",
-    width: "100%",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#000000",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 10,
+    },
+    main: {
+        backgroundColor: "#000000",
+        alignItems: "center",
+        width: "100%",
+    },
+    h1: {
+        color: "#AEAEB2",
+        fontFamily: "Medium",
+        fontSize: 24,
+        textAlign: "center",
+        marginTop: -180,
+        paddingHorizontal: 10,
+    },
+    input: {
+        width: "100%",
+        fontFamily: "Medium",
+        fontSize: 17,
+        borderStyle: "solid",
+        borderWidth: 0.5,
+        borderBottomColor: "rgba(84,84,88,.65)",
+        paddingLeft: 10,
+        paddingBottom: 10,
+        paddingTop: 10,
+        marginBottom: 125,
+        color: "#FFFFFF",
+    },
+    already: {
+        color: "rgba(0,122,255, 100)",
+        fontSize: 14,
+        fontFamily: "Bold",
+        paddingTop: 20,
+        paddingBottom: 15,
+        marginBottom: 70,
+        textAlign: "center",
+    },
+    btn: {
+        backgroundColor: "rgba(255,255,255,0.2)",
+        fontFamily: "Semibold",
+        fontSize: 17,
+        textAlign: "center",
+        paddingVertical: 15,
+        paddingHorizontal: 130,
+        borderRadius: 20,
+        overflow: "hidden",
+        width: "100%",
+    },
+    btncontainer: {
+        position: "absolute",
+        bottom: 0,
+        width: "100%",
+    },
+    btnActive: {
+        backgroundColor: "rgba(255,255,255,100)",
+        fontFamily: "Semibold",
+        fontSize: 17,
+        textAlign: "center",
+        paddingVertical: 15,
+        paddingHorizontal: 130,
+        borderRadius: 20,
+        overflow: "hidden",
+        width: "100%",
+    },
 });
 
 export default SignupEmail;
