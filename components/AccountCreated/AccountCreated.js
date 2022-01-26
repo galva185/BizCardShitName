79f19269acaff23c721fae3cc40a75d8410aa27a
reygranda <reygdev@gmail.com>
2022-01-25 20:54:34 -0500

    Added firebase

--- components/AccountCreated/AccountCreated.js
+++ components/AccountCreated/AccountCreated.js
@@ -1,175 +1,173 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    Keyboard,
-    TouchableOpacity,
-    KeyboardAvoidingView,
-    ScrollView,
-    TextInput,
-    Platform,
-} from "react-native";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useFonts } from "expo-font";
-import { ColorAndroid } from "react-native/Libraries/StyleSheet/PlatformColorValueTypesAndroid";
-import { withSafeAreaInsets } from "react-native-safe-area-context";
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Keyboard,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  ScrollView,
+  TextInput,
+  Platform,
+} from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { useFonts } from 'expo-font';
+import { ColorAndroid } from 'react-native/Libraries/StyleSheet/PlatformColorValueTypesAndroid';
+import { withSafeAreaInsets } from 'react-native-safe-area-context';
 
 const AccountCreated = (props) => {
-    const { route, navigation } = props;
-    const email = "leo@gmail.com";
-    const { password } = "password123";
-    const firstname = "leo"; //change these back to the curly braces ADIOADBADIB
-    const { lastname } = "pants";
-    const { phonenumber } = "786506";
-    //const { email, password, firstname, lastname, phonenumber } = route.params;
-    console.log(email);
+  const { route, navigation } = props;
+  const email = 'leo@gmail.com';
+  const { password } = 'password123';
+  const firstname = 'leo'; //change these back to the curly braces
+  const { lastname } = 'pants';
+  const { phonenumber } = '786506';
+  //const { email, password, firstname, lastname, phonenumber } = route.params;
+  console.log(email);
 
-    const [loaded] = useFonts({
-        Regular: require("../../assets/fonts/SF-Pro-Display-Regular.otf"),
-        Semibold: require("../../assets/fonts/SF-Pro-Display-Semibold.otf"),
-        Bold: require("../../assets/fonts/SF-Pro-Display-Bold.otf"),
-        Medium: require("../../assets/fonts/SF-Pro-Display-Medium.otf"),
-    });
+  const [loaded] = useFonts({
+    Regular: require('../../assets/fonts/SF-Pro-Display-Regular.otf'),
+    Semibold: require('../../assets/fonts/SF-Pro-Display-Semibold.otf'),
+    Bold: require('../../assets/fonts/SF-Pro-Display-Bold.otf'),
+    Medium: require('../../assets/fonts/SF-Pro-Display-Medium.otf'),
+  });
 
-    return (
-        <KeyboardAwareScrollView
-            keyboardShouldPersistTaps="always"
-            scrollEnabled={false}
-            bounces={false}
-            contentContainerStyle={styles.container}
+  return (
+    <KeyboardAwareScrollView
+      keyboardShouldPersistTaps="always"
+      scrollEnabled={false}
+      bounces={false}
+      contentContainerStyle={styles.container}
+    >
+      <StatusBar style="light" />
+      <View style={styles.main}>
+        <View>
+          <View style={styles.imageContainer}>
+            <Image source={require('../../assets/Avatar.png')} />
+          </View>
+          <View>
+            <Text style={styles.h1}>Congratulations {firstname}!</Text>
+          </View>
+          <View style={styles.subtextview}>
+            <Text style={styles.subtext}>
+              You have successfully created your account, press continue to
+              access your BizCards.
+            </Text>
+          </View>
+        </View>
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('Home', {
+              email: email,
+              password: password,
+              firstname: firstname,
+              lastname: lastname,
+              phonenumber: phonenumber,
+            })
+          }
         >
-            <StatusBar style="light" />
-            <View style={styles.main}>
-                <View>
-                    <View style={styles.imageContainer}>
-                        <Image source={require("../../assets/Avatar.png")} />
-                    </View>
-                    <View>
-                        <Text style={styles.h1}>
-                            Congratulations {firstname}!
-                        </Text>
-                    </View>
-                    <View style={styles.subtextview}>
-                        <Text style={styles.subtext}>
-                            You have successfully created your account, press
-                            continue to access your BizCards.
-                        </Text>
-                    </View>
-                </View>
-                <TouchableOpacity
-                    onPress={() =>
-                        navigation.navigate("Home", {
-                            email: email,
-                            password: password,
-                            firstname: firstname,
-                            lastname: lastname,
-                            phonenumber: phonenumber,
-                        })
-                    }
-                >
-                    <Text style={styles.btnActive}>Continue</Text>
-                </TouchableOpacity>
-            </View>
-        </KeyboardAwareScrollView>
-    );
+          <Text style={styles.btnActive}>Continue</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardAwareScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#0D1120",
-        alignItems: "center",
-        justifyContent: "center",
-        padding: 10,
-    },
-    main: {
-        backgroundColor: "#0D1120",
-        alignItems: "center",
-        width: "100%",
-    },
-    h1: {
-        color: "#fff",
-        fontFamily: "Bold",
-        fontWeight: "bold",
-        fontSize: 30,
-        textAlign: "center",
-        marginTop: "40%",
-        paddingHorizontal: 10,
-    },
-    input: {
-        width: "100%",
-        fontFamily: "Medium",
-        fontSize: 17,
-        borderStyle: "solid",
-        borderWidth: 0.5,
-        borderBottomColor: "rgba(84,84,88,.65)",
-        paddingLeft: 10,
-        paddingBottom: 10,
-        paddingTop: 10,
-        marginBottom: 125,
-        color: "#FFFFFF",
-    },
-    already: {
-        color: "rgba(0,122,255, 100)",
-        fontSize: 14,
-        fontFamily: "Bold",
-        paddingTop: 20,
-        paddingBottom: 15,
-        marginBottom: 70,
-        textAlign: "center",
-    },
-    btn: {
-        backgroundColor: "rgba(255,255,255,0.2)",
-        fontFamily: "Semibold",
-        fontSize: 17,
-        textAlign: "center",
-        paddingVertical: 15,
-        paddingHorizontal: 130,
-        borderRadius: 20,
-        overflow: "hidden",
-        width: "100%",
-    },
-    btncontainer: {
-        position: "absolute",
-        bottom: 0,
-        width: "100%",
-    },
-    btnActive: {
-        backgroundColor: "rgba(255,255,255,100)",
-        fontFamily: "Semibold",
-        fontSize: 17,
-        textAlign: "center",
-        paddingVertical: 15,
-        paddingHorizontal: 130,
-        borderRadius: 20,
-        overflow: "hidden",
-        width: "100%",
-    },
-    subtext: {
-        color: "rgba(174,174,178,1.0)",
-        textAlign: "center",
-        fontSize: 18,
-        fontWeight: "500",
-        fontFamily: "Bold",
-    },
-    subtextview: {
-        marginBottom: "50%",
-        marginTop: "10%",
-        width: "80%",
-        textAlign: "center",
-        alignContent: "center",
-    },
-    imageContainer: {
-        marginTop: "25%",
-        justifyContent: "center",
-        alignContent: "center",
-        alignItems: "center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#0D1120',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+  main: {
+    backgroundColor: '#0D1120',
+    alignItems: 'center',
+    width: '100%',
+  },
+  h1: {
+    color: '#fff',
+    fontFamily: 'Bold',
+    fontWeight: 'bold',
+    fontSize: 30,
+    textAlign: 'center',
+    marginTop: '40%',
+    paddingHorizontal: 10,
+  },
+  input: {
+    width: '100%',
+    fontFamily: 'Medium',
+    fontSize: 17,
+    borderStyle: 'solid',
+    borderWidth: 0.5,
+    borderBottomColor: 'rgba(84,84,88,.65)',
+    paddingLeft: 10,
+    paddingBottom: 10,
+    paddingTop: 10,
+    marginBottom: 125,
+    color: '#FFFFFF',
+  },
+  already: {
+    color: 'rgba(0,122,255, 100)',
+    fontSize: 14,
+    fontFamily: 'Bold',
+    paddingTop: 20,
+    paddingBottom: 15,
+    marginBottom: 70,
+    textAlign: 'center',
+  },
+  btn: {
+    backgroundColor: 'rgba(255,255,255,0.2)',
+    fontFamily: 'Semibold',
+    fontSize: 17,
+    textAlign: 'center',
+    paddingVertical: 15,
+    paddingHorizontal: 130,
+    borderRadius: 20,
+    overflow: 'hidden',
+    width: '100%',
+  },
+  btncontainer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+  },
+  btnActive: {
+    backgroundColor: 'rgba(255,255,255,100)',
+    fontFamily: 'Semibold',
+    fontSize: 17,
+    textAlign: 'center',
+    paddingVertical: 15,
+    paddingHorizontal: 130,
+    borderRadius: 20,
+    overflow: 'hidden',
+    width: '100%',
+  },
+  subtext: {
+    color: 'rgba(174,174,178,1.0)',
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: '500',
+    fontFamily: 'Bold',
+  },
+  subtextview: {
+    marginBottom: '50%',
+    marginTop: '10%',
+    width: '80%',
+    textAlign: 'center',
+    alignContent: 'center',
+  },
+  imageContainer: {
+    marginTop: '25%',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+  },
 });
 
 export default AccountCreated;
--- components/Signup/Signup.js
+++ components/Signup/Signup.js
@@ -1,210 +1,213 @@
-import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    TextInput,
-    Button,
-    TouchableOpacity,
-} from "react-native";
-import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 import {
-    NavigationContainer,
-    CommonActions,
-    StackActions,
-} from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-const axios = require("axios");
+  NavigationContainer,
+  CommonActions,
+  StackActions,
+} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { useAuth } from '../contexts/AuthContext';
+const axios = require('axios');
 
 const Signup = (props) => {
-    const { navigation } = props;
-    const [email, setEmail] = useState("");
-    const [userpassword, setPassword] = useState("");
-    const [firstname, setFirstName] = useState(null);
-    const [lastname, setLastName] = useState(null);
-    const [username, setUsername] = useState(null);
-    const [password2, setPassword2] = useState("");
+  const { navigation } = props;
+  const [email, setEmail] = useState('');
+  const [userpassword, setPassword] = useState('');
+  const [firstname, setFirstName] = useState(null);
+  const [lastname, setLastName] = useState(null);
+  const [username, setUsername] = useState(null);
+  const [password2, setPassword2] = useState('');
 
-    const handlePress = (event) => {
-        console.log(email);
-        if (userpassword === password2) {
-            axios({
-                method: "post",
-                url: "https://business-card-backend-qkym9.ondigitalocean.app/createuser",
-                data: {
-                    firstname: firstname,
-                    lastname: lastname,
-                    email: email,
-                    userpassword: userpassword,
-                    username: username,
-                },
-            })
-                .then(function (response) {
-                    console.log(response.request);
-                    navigation.navigate("AddBook");
-                })
-                .catch(function (error) {
-                    if (error.response) {
-                        // Request made and server responded
-                        if (error.response.data === "Username is taken") {
-                            setUsernameError(true);
-                        } else if (error.response.data === "Email is taken") {
-                            setEmailError(true);
-                        }
-                    } else if (error.request) {
-                        // The request was made but no response was received
-                        console.log(error.request);
-                    } else {
-                        // Something happened in setting up the request that triggered an Error
-                        console.log("Error", error.message);
-                    }
-                });
-        } else {
-            setPasswordError(true);
-        }
-        setEmail(null);
-        setPassword("");
-        setPassword2("");
-        setFirstName("");
-        setLastName("");
-        setUsername("");
-    };
+  const { signup } = useAuth();
 
-    return (
-        <KeyboardAwareScrollView contentContainerStyle={styles.container}>
-            <StatusBar style="auto" />
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.TextInput}
-                    placeholder="Username"
-                    placeholderTextColor="#003f5c"
-                    autoCorrect={false}
-                    onChangeText={(username) => setUsername(username)}
-                />
-            </View>
+  const handlePress = (event) => {
+    console.log(email);
+    if (userpassword === password2) {
+      axios({
+        method: 'post',
+        url: 'https://business-card-backend-qkym9.ondigitalocean.app/createuser',
+        data: {
+          firstname: firstname,
+          lastname: lastname,
+          email: email,
+          userpassword: userpassword,
+          username: username,
+        },
+      })
+        .then(function (response) {
+          console.log(response.request);
+          navigation.navigate('AddBook');
+        })
+        .catch(function (error) {
+          if (error.response) {
+            // Request made and server responded
+            if (error.response.data === 'Username is taken') {
+              setUsernameError(true);
+            } else if (error.response.data === 'Email is taken') {
+              setEmailError(true);
+            }
+          } else if (error.request) {
+            // The request was made but no response was received
+            console.log(error.request);
+          } else {
+            // Something happened in setting up the request that triggered an Error
+            console.log('Error', error.message);
+          }
+        });
+    } else {
+      setPasswordError(true);
+    }
+    setEmail(null);
+    setPassword('');
+    setPassword2('');
+    setFirstName('');
+    setLastName('');
+    setUsername('');
+  };
 
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.TextInput}
-                    placeholder="First Name"
-                    placeholderTextColor="#003f5c"
-                    autoCorrect={false}
-                    onChangeText={(firstName) => setFirstName(firstName)}
-                />
-            </View>
+  return (
+    <KeyboardAwareScrollView contentContainerStyle={styles.container}>
+      <StatusBar style="auto" />
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Username"
+          placeholderTextColor="#003f5c"
+          autoCorrect={false}
+          onChangeText={(username) => setUsername(username)}
+        />
+      </View>
 
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.TextInput}
-                    placeholder="Last Name"
-                    placeholderTextColor="#003f5c"
-                    autoCorrect={false}
-                    onChangeText={(lastName) => setLastName(lastName)}
-                />
-            </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="First Name"
+          placeholderTextColor="#003f5c"
+          autoCorrect={false}
+          onChangeText={(firstName) => setFirstName(firstName)}
+        />
+      </View>
 
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.TextInput}
-                    placeholder="Email"
-                    placeholderTextColor="#003f5c"
-                    autoCompleteType="email"
-                    keyboardType="email-address"
-                    autoCorrect={false}
-                    textContentType="emailAddress"
-                    onChangeText={(email) => setEmail(email)}
-                />
-            </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Last Name"
+          placeholderTextColor="#003f5c"
+          autoCorrect={false}
+          onChangeText={(lastName) => setLastName(lastName)}
+        />
+      </View>
 
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.TextInput}
-                    placeholder="Password"
-                    placeholderTextColor="#003f5c"
-                    autoCorrect={false}
-                    secureTextEntry={true}
-                    onChangeText={(password) => setPassword(password)}
-                />
-            </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Email"
+          placeholderTextColor="#003f5c"
+          autoCompleteType="email"
+          keyboardType="email-address"
+          autoCorrect={false}
+          textContentType="emailAddress"
+          onChangeText={(email) => setEmail(email)}
+        />
+      </View>
 
-            <View style={styles.inputView}>
-                <TextInput
-                    style={styles.TextInput}
-                    placeholder="Retype Password"
-                    placeholderTextColor="#003f5c"
-                    autoCorrect={false}
-                    secureTextEntry={true}
-                    onChangeText={(password2) => setPassword2(password2)}
-                />
-            </View>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Password"
+          placeholderTextColor="#003f5c"
+          autoCorrect={false}
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
+      </View>
 
-            <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-                <Text style={styles.forgot_button}>
-                    Already have an account? Log In
-                </Text>
-            </TouchableOpacity>
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Retype Password"
+          placeholderTextColor="#003f5c"
+          autoCorrect={false}
+          secureTextEntry={true}
+          onChangeText={(password2) => setPassword2(password2)}
+        />
+      </View>
 
-            <TouchableOpacity
-                style={styles.loginBtn}
-                onPress={() => {
-                    handlePress();
-                }}
-            >
-                <Text style={styles.loginText}>SIGNUP</Text>
-            </TouchableOpacity>
-        </KeyboardAwareScrollView>
-    );
+      <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+        <Text style={styles.forgot_button}>
+          Already have an account? Log In
+        </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.loginBtn}
+        onPress={() => {
+          handlePress();
+        }}
+      >
+        <Text style={styles.loginText}>SIGNUP</Text>
+      </TouchableOpacity>
+    </KeyboardAwareScrollView>
+  );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 
-    image: {
-        marginBottom: 0,
-        resizeMode: "contain",
-        width: "40%",
-    },
+  image: {
+    marginBottom: 0,
+    resizeMode: 'contain',
+    width: '40%',
+  },
 
-    inputView: {
-        backgroundColor: "#eee",
-        borderRadius: 8,
-        width: "80%",
-        height: 60,
-        marginBottom: 10,
-        alignItems: "flex-start",
-        justifyContent: "flex-start",
-        paddingLeft: "5%",
-    },
+  inputView: {
+    backgroundColor: '#eee',
+    borderRadius: 8,
+    width: '80%',
+    height: 60,
+    marginBottom: 10,
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    paddingLeft: '5%',
+  },
 
-    TextInput: {
-        height: 50,
-        flex: 1,
-        padding: 10,
-        color: "#21b58b",
-    },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    color: '#21b58b',
+  },
 
-    forgot_button: {
-        height: 30,
-        marginBottom: 0,
-        marginTop: 3,
-    },
+  forgot_button: {
+    height: 30,
+    marginBottom: 0,
+    marginTop: 3,
+  },
 
-    loginBtn: {
-        width: "80%",
-        borderRadius: 8,
-        height: 50,
-        alignItems: "center",
-        justifyContent: "center",
-        marginTop: 15,
-        backgroundColor: "#21b58b",
-    },
+  loginBtn: {
+    width: '80%',
+    borderRadius: 8,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 15,
+    backgroundColor: '#21b58b',
+  },
 });
 
 export default Signup;
